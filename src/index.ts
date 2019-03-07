58c487c01da339fb9ddbf309f041cd64e5cecba3
Michael Lavers <kolanos@gmail.com>
2019-03-06 20:24:21 -0800

    Add prettier since tslint gets indents wrong

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,34 @@ export class IOpipeLayerPlugin {
   public serverless: Serverless;
   public options: Serverless.Options;
   public hooks: {
-   [event: string]: Promise<any>;
+    [event: string]: Promise<any>;
   };
 
-  constructor(serverless: Serverless , options: Serverless.Options) {
+  constructor(serverless: Serverless, options: Serverless.Options) {
     this.serverless = serverless;
     this.options = options;
     this.hooks = {
-     "before:package:createDeploymentArtifacts": this.run.bind(this),
+      "before:package:createDeploymentArtifacts": this.run.bind(this)
     };
   }
 
   get functions() {
-    return this.serverless.service.getAllFunctions().map((func) => this.serverless.service.getFunction(func));
+    return this.serverless.service
+      .getAllFunctions()
+      .map(func => this.serverless.service.getFunction(func));
   }
 
   public run() {
-    this.functions.forEach((func) => {
- const { runtime, layers = [] } = func as any;
- this.addLayer(runtime, layers);
- this.serverless.cli.log(`${JSON.stringify(func)}`);
+    this.functions.forEach(func => {
+      const { runtime, layers = [] } = func as any;
+      this.addLayer(runtime, layers);
+      this.serverless.cli.log(`${JSON.stringify(func)}`);
     });
- }
+  }
 
- private addLayer(runtime: string, layers: string[]) {
-  layers.push("arn:foo:bar");
- }
+  private addLayer(runtime: string, layers: string[]) {
+    layers.push("arn:foo:bar");
+  }
 }
 
 module.exports = IOpipeLayerPlugin;
