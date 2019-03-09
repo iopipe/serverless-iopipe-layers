895398b34ac8230abd2552e310a9acbf275146a9
Michael Lavers <kolanos@gmail.com>
2019-03-08 16:30:20 -0800

    Injecting layer ARNs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import * as _ from "lodash";
 import * as Serverless from "serverless";
+
+import * as layerArns from "./layers";
 
 export class IOpipeLayerPlugin {
   public serverless: Serverless;
@@ -9,28 +12,98 @@ export class IOpipeLayerPlugin {
 
   constructor(serverless: Serverless, options: Serverless.Options) {
     this.serverless = serverless;
+
     this.options = options;
+
     this.hooks = {
       "before:package:createDeploymentArtifacts": this.run.bind(this)
     };
   }
 
   get functions() {
-    return this.serverless.service
-      .getAllFunctions()
-      .map(func => this.serverless.service.getFunction(func));
+    return Object.assign.apply(
+      null,
+      this.serverless.service
+        .getAllFunctions()
+        .map(func => ({ [func]: this.serverless.service.getFunction(func) }))
+    );
   }
 
   public run() {
-    this.functions.forEach(func => {
-      const { runtime, layers = [] } = func as any;
-      this.addLayer(runtime, layers);
-      this.serverless.cli.log(`${JSON.stringify(func)}`);
+    const funcs = this.functions;
+
+    Object.keys(funcs).forEach(funcName => {
+      const funcDef = funcs[funcName];
+      this.addLayer(funcName, funcDef);
     });
   }
 
-  private addLayer(runtime: string, layers: string[]) {
-    layers.push("arn:foo:bar");
+  private addLayer(funcName: string, funcDef: any) {
+    this.serverless.cli.log(`Adding IOpipe layer to ${funcName}`);
+
+    const region = _.get(this.serverless.service, "provider.region");
+    const {
+      environment = {},
+      handler,
+      runtime = _.get(this.serverless.service, "provider.runtime"),
+      layers = []
+    } = funcDef;
+
+    const iopipeLayers = layers.filter(
+      layer => typeof layer === "string" && layer.match("146318645305")
+    );
+
+    if (iopipeLayers.length) {
+      this.serverless.cli.log(
+        `Function "${funcName}" already specifies an IOpipe layer, skipping`
+      );
+      return;
+    }
+
+    if (
+      typeof runtime !== "string" ||
+      [
+        "nodejs6.10",
+        "nodejs8.10",
+        "python2.7",
+        "python3.6",
+        "python3.7"
+      ].indexOf(runtime) === -1
+    ) {
+      this.serverless.cli.log(
+        `Unsupported runtime "${runtime}" for Iopipe layer, skipping`
+      );
+      return;
+    }
+
+    layers.push(this.getLayerArn(runtime, region));
+    funcDef.layers = layers;
+
+    environment.IOPIPE_HANDLER = handler;
+    funcDef.environment = environment;
+
+    funcDef.handler = this.getHandlerWrapper(runtime, handler);
+
+    this.serverless.cli.log(`${JSON.stringify(funcDef)}`);
+  }
+
+  private getLayerArn(runtime: string, region: string) {
+    return _.chain(layerArns)
+      .get(runtime)
+      .get(region)
+      .value();
+  }
+
+  private getHandlerWrapper(runtime: string, handler: string) {
+    if (runtime.match("nodejs")) {
+      return "@iopipe/iopipe.handler";
+    }
+
+    if (runtime.match("python")) {
+      return "iopipe.handler.wrapper";
+    }
+
+    return handler;
   }
 }
 
---
+++ src/layers.ts
@@ -0,0 +1,131 @@
+const layers = {
+  "nodejs6.10": {
+    "ap-northeast-1":
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipeNodeJS610:4",
+    "ap-northeast-2":
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipeNodeJS610:3",
+    "ap-south-1":
+      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipeNodeJS610:3",
+    "ap-southeast-1":
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipeNodeJS610:3",
+    "ap-southeast-2":
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipeNodeJS610:4",
+    "ca-central-1":
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipeNodeJS610:3",
+    "eu-central-1":
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipeNodeJS610:3",
+    "eu-west-1":
+      "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipeNodeJS610:4",
+    "eu-west-2":
+      "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipeNodeJS610:3",
+    "eu-west-3":
+      "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipeNodeJS610:3",
+    "us-east-1":
+      "arn:aws:lambda:us-east-1:146318645305:layer:IOpipeNodeJS610:4",
+    "us-east-2":
+      "arn:aws:lambda:us-east-2:146318645305:layer:IOpipeNodeJS610:4",
+    "us-west-1":
+      "arn:aws:lambda:us-west-1:146318645305:layer:IOpipeNodeJS610:4",
+    "us-west-2": "arn:aws:lambda:us-west-2:146318645305:layer:IOpipeNodeJS610:4"
+  },
+  "nodejs8.10": {
+    "ap-northeast-1":
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipeNodeJS810:4",
+    "ap-northeast-2":
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipeNodeJS810:3",
+    "ap-south-1":
+      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipeNodeJS810:3",
+    "ap-southeast-1":
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipeNodeJS810:3",
+    "ap-southeast-2":
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipeNodeJS810:4",
+    "ca-central-1":
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipeNodeJS810:3",
+    "eu-central-1":
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipeNodeJS810:3",
+    "eu-west-1":
+      "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipeNodeJS810:4",
+    "eu-west-2":
+      "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipeNodeJS810:3",
+    "eu-west-3":
+      "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipeNodeJS810:3",
+    "us-east-1":
+      "arn:aws:lambda:us-east-1:146318645305:layer:IOpipeNodeJS810:4",
+    "us-east-2":
+      "arn:aws:lambda:us-east-2:146318645305:layer:IOpipeNodeJS810:4",
+    "us-west-1":
+      "arn:aws:lambda:us-west-1:146318645305:layer:IOpipeNodeJS810:4",
+    "us-west-2": "arn:aws:lambda:us-west-2:146318645305:layer:IOpipeNodeJS810:4"
+  },
+  "python2.7": {
+    "ap-northeast-1":
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipePython27:10",
+    "ap-northeast-2":
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipePython27:8",
+    "ap-south-1":
+      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipePython27:8",
+    "ap-southeast-1":
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipePython27:8",
+    "ap-southeast-2":
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipePython27:8",
+    "ca-central-1":
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipePython27:8",
+    "eu-central-1":
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipePython27:8",
+    "eu-west-1": "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipePython27:8",
+    "eu-west-2": "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipePython27:8",
+    "eu-west-3": "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipePython27:8",
+    "us-east-1": "arn:aws:lambda:us-east-1:146318645305:layer:IOpipePython27:8",
+    "us-east-2": "arn:aws:lambda:us-east-2:146318645305:layer:IOpipePython27:8",
+    "us-west-1": "arn:aws:lambda:us-west-1:146318645305:layer:IOpipePython27:8",
+    "us-west-2": "arn:aws:lambda:us-west-2:146318645305:layer:IOpipePython27:8"
+  },
+  "python3.6": {
+    "ap-northeast-1":
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipePython:8",
+    "ap-northeast-2":
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipePython:8",
+    "ap-south-1": "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipePython:8",
+    "ap-southeast-1":
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipePython:8",
+    "ap-southeast-2":
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipePython:8",
+    "ca-central-1":
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipePython:8",
+    "eu-central-1":
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipePython:8",
+    "eu-west-1": "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipePython:8",
+    "eu-west-2": "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipePython:8",
+    "eu-west-3": "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipePython:8",
+    "us-east-1": "arn:aws:lambda:us-east-1:146318645305:layer:IOpipePython:9",
+    "us-east-2": "arn:aws:lambda:us-east-2:146318645305:layer:IOpipePython:8",
+    "us-west-1": "arn:aws:lambda:us-west-1:146318645305:layer:IOpipePython:8",
+    "us-west-2": "arn:aws:lambda:us-west-2:146318645305:layer:IOpipePython:8"
+  },
+  "python3.7": {
+    "ap-northeast-1":
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipePython:8",
+    "ap-northeast-2":
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipePython:8",
+    "ap-south-1": "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipePython:8",
+    "ap-southeast-1":
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipePython:8",
+    "ap-southeast-2":
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipePython:8",
+    "ca-central-1":
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipePython:8",
+    "eu-central-1":
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipePython:8",
+    "eu-west-1": "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipePython:8",
+    "eu-west-2": "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipePython:8",
+    "eu-west-3": "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipePython:8",
+    "us-east-1": "arn:aws:lambda:us-east-1:146318645305:layer:IOpipePython:9",
+    "us-east-2": "arn:aws:lambda:us-east-2:146318645305:layer:IOpipePython:8",
+    "us-west-1": "arn:aws:lambda:us-west-1:146318645305:layer:IOpipePython:8",
+    "us-west-2": "arn:aws:lambda:us-west-2:146318645305:layer:IOpipePython:8"
+  }
+};
+
+export default layers;
+
+module.exports = layers;
