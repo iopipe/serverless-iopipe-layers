196a41a8bccb8305c9e2fe3f93b73c760ceb6b0f
Michael Lavers <kolanos@gmail.com>
2019-06-19 11:22:28 -0700

    Updating Node Layer ARNs

--- src/layers.ts
+++ src/layers.ts
@@ -1,63 +1,63 @@
 const layers = {
   "nodejs6.10": {
     "ap-northeast-1":
-      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipeNodeJS610:10",
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipeNodeJS610:11",
     "ap-northeast-2":
-      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipeNodeJS610:10",
     "ap-south-1":
-      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipeNodeJS610:10",
     "ap-southeast-1":
-      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipeNodeJS610:10",
     "ap-southeast-2":
-      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipeNodeJS610:10",
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipeNodeJS610:11",
     "ca-central-1":
-      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipeNodeJS610:10",
     "eu-central-1":
-      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipeNodeJS610:10",
     "eu-west-1":
-      "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipeNodeJS610:10",
+      "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipeNodeJS610:11",
     "eu-west-2":
-      "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipeNodeJS610:10",
     "eu-west-3":
-      "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipeNodeJS610:9",
+      "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipeNodeJS610:10",
     "us-east-1":
-      "arn:aws:lambda:us-east-1:146318645305:layer:IOpipeNodeJS610:10",
+      "arn:aws:lambda:us-east-1:146318645305:layer:IOpipeNodeJS610:11",
     "us-east-2":
-      "arn:aws:lambda:us-east-2:146318645305:layer:IOpipeNodeJS610:10",
+      "arn:aws:lambda:us-east-2:146318645305:layer:IOpipeNodeJS610:11",
     "us-west-1":
-      "arn:aws:lambda:us-west-1:146318645305:layer:IOpipeNodeJS610:10",
+      "arn:aws:lambda:us-west-1:146318645305:layer:IOpipeNodeJS610:11",
     "us-west-2":
-      "arn:aws:lambda:us-west-2:146318645305:layer:IOpipeNodeJS610:10"
+      "arn:aws:lambda:us-west-2:146318645305:layer:IOpipeNodeJS610:11"
   },
   "nodejs8.10": {
     "ap-northeast-1":
-      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipeNodeJS810:10",
+      "arn:aws:lambda:ap-northeast-1:146318645305:layer:IOpipeNodeJS810:11",
     "ap-northeast-2":
-      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:ap-northeast-2:146318645305:layer:IOpipeNodeJS810:10",
     "ap-south-1":
-      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:ap-south-1:146318645305:layer:IOpipeNodeJS810:10",
     "ap-southeast-1":
-      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:ap-southeast-1:146318645305:layer:IOpipeNodeJS810:10",
     "ap-southeast-2":
-      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipeNodeJS810:10",
+      "arn:aws:lambda:ap-southeast-2:146318645305:layer:IOpipeNodeJS810:11",
     "ca-central-1":
-      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:ca-central-1:146318645305:layer:IOpipeNodeJS810:10",
     "eu-central-1":
-      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:eu-central-1:146318645305:layer:IOpipeNodeJS810:10",
     "eu-west-1":
-      "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipeNodeJS810:10",
+      "arn:aws:lambda:eu-west-1:146318645305:layer:IOpipeNodeJS810:11",
     "eu-west-2":
-      "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:eu-west-2:146318645305:layer:IOpipeNodeJS810:10",
     "eu-west-3":
-      "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipeNodeJS810:9",
+      "arn:aws:lambda:eu-west-3:146318645305:layer:IOpipeNodeJS810:10",
     "us-east-1":
-      "arn:aws:lambda:us-east-1:146318645305:layer:IOpipeNodeJS810:10",
+      "arn:aws:lambda:us-east-1:146318645305:layer:IOpipeNodeJS810:11",
     "us-east-2":
-      "arn:aws:lambda:us-east-2:146318645305:layer:IOpipeNodeJS810:10",
+      "arn:aws:lambda:us-east-2:146318645305:layer:IOpipeNodeJS810:11",
     "us-west-1":
-      "arn:aws:lambda:us-west-1:146318645305:layer:IOpipeNodeJS810:10",
+      "arn:aws:lambda:us-west-1:146318645305:layer:IOpipeNodeJS810:11",
     "us-west-2":
-      "arn:aws:lambda:us-west-2:146318645305:layer:IOpipeNodeJS810:10"
+      "arn:aws:lambda:us-west-2:146318645305:layer:IOpipeNodeJS810:11"
   },
   "python2.7": {
     "ap-northeast-1":
